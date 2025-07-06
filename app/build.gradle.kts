90eaa5c169d5fce7f8c4e169f06d379a8297d0b4
collins333 <collins.mdfp@gmail.com>
2025-07-06 19:37:23 +0200

    permisos camara

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 }
 
 dependencies {
+    val cameraVersion = "1.4.2"
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
@@ -61,6 +62,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.activity.ktx)
 
     //Navigation component
     implementation(libs.androidx.navigation.fragment)
@@ -75,5 +77,10 @@ dependencies {
     implementation (libs.converter.gson)
     implementation (libs.logging.interceptor)
 
-    implementation(libs.androidx.activity.ktx)
+    //Camera X
+    implementation (libs.androidx.camera.core)
+    implementation (libs.androidx.camera.camera2)
+    implementation (libs.androidx.camera.lifecycle)
+    implementation (libs.androidx.camera.view)
+    implementation (libs.androidx.camera.extensions)
 }
--- app/src/main/java/com/collins/cursoandroid/horoscopoaplicacion/ui/quiromancia/QuiromanciaFragment.kt
+++ app/src/main/java/com/collins/cursoandroid/horoscopoaplicacion/ui/quiromancia/QuiromanciaFragment.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.collins.cursoandroid.horoscopoaplicacion.databinding.FragmentQuiromanciaBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,8 +15,39 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class QuiromanciaFragment : Fragment() {
 
+    companion object {
+        private const val PERMISO_CAMARA = android.Manifest.permission.CAMERA
+    }
+
     private var _binding: FragmentQuiromanciaBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        isGranted: Boolean ->
+
+        if(isGranted) {
+            //startCamera
+        } else {
+            Toast.makeText(requireContext(), "Acepta los permisos para poder usar la aplicación", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(comprobarPermisoCamara()) {
+            // tiene permiso aceptaqdo
+            //startCamera
+        } else {
+            requestPermissionLauncher.launch(PERMISO_CAMARA)
+        }
+    }
+
+    private fun comprobarPermisoCamara(): Boolean {
+        return PermissionChecker.checkSelfPermission(
+            requireContext(), PERMISO_CAMARA
+        ) == PermissionChecker.PERMISSION_GRANTED
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
