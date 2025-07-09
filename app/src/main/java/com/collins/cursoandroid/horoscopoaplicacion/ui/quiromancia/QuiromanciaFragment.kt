53df7ca5a8e2716242b578ccd9c2b73d1605c308
collins333 <collins.mdfp@gmail.com>
2025-07-09 12:01:24 +0200

    quiromancia

--- app/src/main/java/com/collins/cursoandroid/horoscopoaplicacion/ui/quiromancia/QuiromanciaFragment.kt
+++ app/src/main/java/com/collins/cursoandroid/horoscopoaplicacion/ui/quiromancia/QuiromanciaFragment.kt
@@ -1,15 +1,21 @@
 package com.collins.cursoandroid.horoscopoaplicacion.ui.quiromancia
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.registerForActivityResult
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.collins.cursoandroid.horoscopoaplicacion.databinding.FragmentQuiromanciaBinding
+import com.google.common.util.concurrent.ListenableFuture
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,7 +32,7 @@ class QuiromanciaFragment : Fragment() {
         isGranted: Boolean ->
 
         if(isGranted) {
-            //startCamera
+            startCamera()
         } else {
             Toast.makeText(requireContext(), "Acepta los permisos para poder usar la aplicación", Toast.LENGTH_LONG).show()
         }
@@ -36,11 +42,35 @@ class QuiromanciaFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         if(comprobarPermisoCamara()) {
-            // tiene permiso aceptaqdo
-            //startCamera
+            startCamera()
         } else {
             requestPermissionLauncher.launch(PERMISO_CAMARA)
         }
+    }
+
+    private fun startCamera() {
+        val proveedorCamara: ListenableFuture<ProcessCameraProvider> = ProcessCameraProvider.getInstance(requireContext())
+
+        proveedorCamara.addListener({
+            val camaraProveedor: ProcessCameraProvider = proveedorCamara.get()
+
+            val previo: Preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.pvBuscador.surfaceProvider)
+                }
+
+            val selectorCamara = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                camaraProveedor.unbindAll()
+
+                camaraProveedor.bindToLifecycle(this, selectorCamara, previo)
+            } catch (e: Exception) {
+                Log.i("prueba", "algo fue mal: ${e.message}")
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
     private fun comprobarPermisoCamara(): Boolean {
